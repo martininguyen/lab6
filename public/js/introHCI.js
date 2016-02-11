764bb77f967f40b15ae1c63cb5a9169ca1b94af6
Duy Nguyen <martinnguyen0618@gmail.com>
2016-02-11 01:14:09 -0800

    finished AJAX lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,6 +25,17 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	
+	$.get("/project/" + idNumber, addProj);
+	function addProj(result){
+		console.log(result);
+		var data = '<a href="#" class="thumbnail">' +
+			'<img src="' + result['image'] + '"class="detailsImage">' +
+			'<p><small>' + result['title'] + '</small></p>' +
+			'<p><small>' + result['date'] + '</small></p></a>';
+			
+		$("#" + projectID + " .details").html(data+result['summary']);
+	}
 
 	console.log("User clicked on project " + idNumber);
 }
@@ -35,4 +46,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	
+	$.get("/palette", randColor);
+	function randColor(result){
+		console.log(result);
+		var colors = result['colors']['hex'];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	}
+	
 }
